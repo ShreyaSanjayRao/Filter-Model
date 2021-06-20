320b4450894f897b043733dba0f42c3ba975a0f7
ShreyaSanjayRao <73601962+ShreyaSanjayRao@users.noreply.github.com>
2021-06-20 12:56:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload()
+{
+
+}
+function setup()
+{
+ canvas=createCanvas(410,410);
+ canvas.center();
+}
+function draw()
+{
+
+}
+function capture ()
+{
+    save("necklaces_image.png");
+}
